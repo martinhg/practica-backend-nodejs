85bcb0f87da719a61d2352002b241edc49714c74
martin <martinherrera.g96@gmail.com>
2020-08-31 13:42:22 -0300

    Cambios en el config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    dbUrl: process.env.DB_URL || 'mongodb+srv://db_user_practica:Gustavo96@cluster0.5diyu.mongodb.net/practica_db?retryWrites=true&w=majority',
+    dbUrl: process.env.DB_URL || 'mongodb+srv://db_user_practica:<password>@cluster0.5diyu.mongodb.net/<db_name>?retryWrites=true&w=majority',
     port: process.env.PORT || 3000,
     host: process.envHOST || 'http://localhost'
 };
